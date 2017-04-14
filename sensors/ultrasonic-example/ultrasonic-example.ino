4902bec66d30273233d9ecb3fd8ee9bf3f8f3567
Tim Brown <tim@redpill.me.uk>
2017-04-14 22:01:31 +0100

    minor update to ultrasonic-example.ino

--- sensors/ultrasonic-example/ultrasonic-example.ino
+++ sensors/ultrasonic-example/ultrasonic-example.ino
@@ -1,7 +1,7 @@
 #include <NewPing.h>
 
-#define TRIGGER_PIN  3
-#define ECHO_PIN     2
+#define ECHO_PIN       2
+#define TRIGGER_PIN    3
 #define MAX_DISTANCE 200
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
