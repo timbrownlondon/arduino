0e41ae04c49257d4fae0f9f7e7070b9693e14470
Tim Brown <tim@redpill.me.uk>
2017-05-13 14:23:16 +0100

    add new file: i2c/software-i2c-test/software-i2c-test.ino

---
+++ i2c/software-i2c-test/software-i2c-test.ino
@@ -0,0 +1,15 @@
+#define SCL_PIN 2 
+#define SCL_PORT PORTD 
+#define SDA_PIN 0 
+#define SDA_PORT PORTC
+#include <SoftI2CMaster.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
