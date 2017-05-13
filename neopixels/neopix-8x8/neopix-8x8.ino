bec450c026b310a4a9d8eb9e049691621eade011
Tim Brown <tim@redpill.me.uk>
2017-05-13 14:21:47 +0100

    add new file: neopixels/neopix-8x8/neopix-8x8.ino

---
+++ neopixels/neopix-8x8/neopix-8x8.ino
@@ -0,0 +1,18 @@
+#include <Adafruit_NeoPixel.h>
+
+#include <gfxfont.h>        // do we need this?
+#include <Adafruit_GFX.h>
+
+#include <gamma.h>
+#include <Adafruit_NeoMatrix.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
