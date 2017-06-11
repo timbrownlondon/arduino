a7f9c86df97fad2e9ef609709c4c51c6586c22e2
Tim Brown <tim@redpill.me.uk>
2017-06-11 21:41:00 +0100

    update led/tm1638-test/tm1638-test.ino

--- led/tm1638-test/tm1638-test.ino
+++ led/tm1638-test/tm1638-test.ino
@@ -1,28 +1,36 @@
 #include <TM1638.h>
 
 
-// define a module on data pin 8, clock pin 9 and strobe pin 10, activate is true, brightness 1
-TM1638 module(8, 9, 10, true, 1);
+// define a module on data pin 8, clock pin 9 and strobe pin 6, activate is true, brightness 1
+TM1638 display_a(8, 9, 6, true, 1);
+TM1638 display_b(8, 9, 7, true, 1);
 unsigned long a = 1;
 
 void setup() {
   byte values[] = { 1, 2, 4, 8, 16, 32, 64, 255, 0 };
-  module.setDisplay(values);
+  display_b.setDisplay(values);
   delay(400);
 
-  module.setDisplayToHexNumber(0x1234ABCD, 0xF0);
-  module.setLED(1, 1);
-  module.setLEDs(255);
-  delay(400);
+  display_b.setDisplayToHexNumber(0x1234ABCD, 0xF0);
+  display_b.setLED(1, 1);
+  display_b.setLEDs(255);
+  //delay(400);
 }
 
 void loop() {
-  module.setDisplayToString("21 Oct 1985");
+  display_b.setDisplayToString("21 Oct 1985");
   delay(5000);
   for (a = 1; a <= 100000000; a++) {
-    module.setDisplayToDecNumber(a, 0, true);
-    module.setLEDs(0);
-    module.setLED(1, a % 8);
+
+    byte led_bit_map = a % 256;
+    display_a.setLEDs(led_bit_map);
+    display_b.setLEDs(256 - led_bit_map);
+    
+    display_a.setDisplayToDecNumber(led_bit_map, 0, false);
+    display_b.setDisplayToDecNumber(256 - led_bit_map, 0, false);
+
+
+
     delay(400);
   }
 }
