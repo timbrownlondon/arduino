36002d082f31ac4c6fec3f8f0eb4d0c3932888a8
Tim Brown <tim@redpill.me.uk>
2017-06-11 22:15:16 +0100

    add basic lcd usage to pir-neo-matrix.ino

--- neopixels/pir-neo-matrix/pir-neo-matrix.ino
+++ neopixels/pir-neo-matrix/pir-neo-matrix.ino
@@ -4,6 +4,29 @@
 #include <gamma.h>
 
 
+
+
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+// refer to...
+// https://arduino-info.wikispaces.com/LCD-Blue-I2C
+// https://github.com/marcoschwartz/LiquidCrystal_I2C
+// NB - also checked out following 
+// https://bitbucket.org/fmalpartida/new-liquidcrystal/downloads/
+// I2C Serial Interface 16X2 Character LCD Display
+// bought 3 from ebay £1.87 each, 14 April 2017
+
+// The standard pins for Uno (according to Wire.h, I guess)
+// SDA = Analog 4 (serial data)
+// SCL = Analog 5 (serial clock)
+
+// contructor(I2C address, columns, rows)
+// the displays I have use I2C address 0x3F
+LiquidCrystal_I2C lcd(0x3F, 16, 2);
+
+
+
 Adafruit_NeoMatrix matrix
   = Adafruit_NeoMatrix(8, 8, 2,
                        NEO_MATRIX_TOP + NEO_MATRIX_RIGHT +
@@ -21,23 +44,14 @@ void setup() {
   matrix.setTextColor(colors[0]);
   matrix.fillScreen(colors[2]);
   matrix.show();
+
+  lcd.init();
+  // Print a message to the LCD.
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Hello");
 }
 
-int x    = matrix.width();
-int pass = 0;
-
-void __loop() {
-  matrix.fillScreen(0);
-  matrix.setCursor(x, 0);
-  matrix.print(F("Howdy"));
-  if (--x < -36) {
-    x = matrix.width();
-    if (++pass >= 3) pass = 0;
-    matrix.setTextColor(colors[pass]);
-  }
-  matrix.show();
-  delay(1000);
-}
 
 void loop() {
   matrix.drawPixel(0, 0, colors[1]);
