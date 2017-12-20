3dc0ce69aa75f22ead8e100d3da30a2a69836fc3
Tim Brown <tim@redpill.me.uk>
2017-12-20 20:47:58 +0000

    tidy wifi/http-server/http-server.ino

--- wifi/http-server/http-server.ino
+++ wifi/http-server/http-server.ino
@@ -6,17 +6,17 @@ WiFiServer server(80); //Initialize the server on Port 80
 
 void setup() {
 
-  WiFi.mode(WIFI_AP);                    // set as an AccessPoint
-  WiFi.softAP("Wemos D1", "12345678");  // Provide the (SSID, password); .
-  server.begin(); // Start the HTTP Server
+  WiFi.mode(WIFI_AP);                   // set as an AccessPoint
+  WiFi.softAP("Wemos D1", "12345678");  // with SSID, password
+  server.begin();                       // start  HTTP server
 
   Serial.begin(115200); //Start communication between the ESP8266-12E and the monitor window
-  IPAddress HTTPS_ServerIP = WiFi.softAPIP(); // Obtain the IP of the Server
-  Serial.print("Server IP is: "); // Print the IP to the monitor window
-  Serial.println(HTTPS_ServerIP);
 
-  pinMode(BUILTIN_LED, OUTPUT);   // BUILTIN_LED appears to be 2
-  digitalWrite(BUILTIN_LED, LOW); // switch on
+  Serial.print("Server IP is: ");
+  Serial.println(WiFi.softAPIP());
+
+  pinMode(BUILTIN_LED, OUTPUT);   // BUILTIN_LED is 2 for arduino D1 board
+  digitalWrite(BUILTIN_LED, LOW); // switch it on
 
 }
 
@@ -25,13 +25,15 @@ void loop() {
   if (!client) {
     return;
   }
-  //Looking under the hood
-  Serial.println("Somebody has connected :)");
+
+  Serial.print("client at ");
+  Serial.println(client.remoteIP());
+  Serial.println();
 
   String request = client.readString();
   Serial.println(request);
 
-  // Handle the Request
+  // handle request
   if (request.indexOf("/OFF") != -1) {
     digitalWrite(BUILTIN_LED, HIGH);
   }
@@ -48,9 +50,6 @@ void loop() {
   s += "<br><input type=\"button\" value=\"turn LED off\" onclick=\"location.href='/OFF'\">";
   s += "</html>\n";
 
-
   client.flush(); //clear previous info in the stream
   client.print(s); // Send the response to the client
-  delay(1);
-  Serial.println("Client disonnected"); //Looking under the hood
 }
