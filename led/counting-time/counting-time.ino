1d425123d9dd4e2cd4acf10a961d35013faae9d6
Tim Brown <tim@redpill.me.uk>
2017-12-21 22:31:19 +0000

    counting-time.ino - read buttons to temporarily set count to powers of ten (minus nine) i.e. test modes and stop when count reaches 99,999,999

--- led/counting-time/counting-time.ino
+++ led/counting-time/counting-time.ino
@@ -1,7 +1,7 @@
 // https://github.com/rjbatista/tm1638-library
 #include <TM1638.h>
 #include <TimerOne.h>
-#include <EEPROM.h>
+#include <EepromAPI.h>
 #include <CountDisplay.h>
 
 // data pin 8, clock pin 9, strobe pin 6/7, activate, brightness 0-7
@@ -14,12 +14,13 @@ CountDisplay counter_1(&board_1);
 CountDisplay counter_2(&board_2);
 
 unsigned long count;
+boolean save_count_to_eeprom = true;
 
 void setup() {
   t.initialize(1000000);       // interrupt every second
   t.attachInterrupt(count_up); // call function on interrupt
 
-  count = readLongAt(0);
+  count = EepromAPI::readLongAt(0);
   Serial.begin(9600);
   Serial.println(count);
 }
@@ -27,41 +28,40 @@ void setup() {
 void loop() {
   counter_1.display_seconds(count);
   counter_2.display_days_hours(count);
+
+  byte button = counter_1.getButton();
+
+  if (button > 0) {
+    save_count_to_eeprom = false;
+
+    unsigned long n = 1;
+    for (int i = button; i > 0; i--) {
+      n *= 10L;
+    }
+    count = n - 9;
+    Serial.println(button);
+    Serial.println(count);
+  }
+
+  // click button 8 on lower display
+  // to reset count to one million
+  if (counter_2.getButton() == 8) {
+    count = 1000000;
+    EepromAPI::writeLongAt(0, count);
+  }
 }
 
 void count_up() {
   count++;
-  // write current value of count to EEPROM when divisible by 256 (every 4mins or so)
-  // the count is reset to that value after a restart
-  if ((count & 0xFF) == 0) {
-    writeLongAt(0, count);
-    Serial.println(count);
+  if (count == 99999999){
+    t.stop();
   }
-}
 
-void writeLongAt(int address, unsigned long value) {
-  //Decomposition from an unsigned long to 4 bytes by using bitshift.
-  byte d = ((value >>  0) & 0xFF);
-  byte c = ((value >>  8) & 0xFF);
-  byte b = ((value >> 16) & 0xFF);
-  byte a = ((value >> 24) & 0xFF);
-
-  // write four bytes to the eeprom memory
-  EEPROM.write(address + 0, a);
-  EEPROM.write(address + 1, b);
-  EEPROM.write(address + 2, c);
-  EEPROM.write(address + 3, d);
-}
-
-//This function will return a 4 byte (32bit) long from the eeprom
-//at the specified address to address + 3.
-long readLongAt(int address) {
-  // read four bytes from the eeprom memory
-  long a = EEPROM.read(address);
-  long b = EEPROM.read(address + 1);
-  long c = EEPROM.read(address + 2);
-  long d = EEPROM.read(address + 3);
-
-  //Return the recomposed long by using bitshift.
-  return ((d << 0) & 0xFF) + ((c << 8) & 0xFFFF) + ((b << 16) & 0xFFFFFF) + ((a << 24) & 0xFFFFFFFF);
+    // write current value of count to EEPROM when divisible by 256 (every 4mins or so)
+    // the count is reset to that value after a restart
+  if ((count & 0xFF) == 0 and save_count_to_eeprom) {
+    EepromAPI::writeLongAt(0, count);
+      Serial.print("save count: ");
+      Serial.println(count);
+    }
 }
