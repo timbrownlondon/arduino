ff8374cc0c2129de99307ec91f7841c7c7b46e4c
Tim Brown <tim@redpill.me.uk>
2018-03-03 17:23:30 +0000

    pir-neopix-interrupt.ino - move the matrix animation from interrupt into loop()

--- neopixels/pir-neopix-interrupt/pir-neopix-interrupt.ino
+++ neopixels/pir-neopix-interrupt/pir-neopix-interrupt.ino
@@ -22,10 +22,7 @@
 // the displays I have use I2C address 0x3F
 LiquidCrystal_I2C lcd(0x3F, 16, 2);
 
-unsigned long button_press_millis = millis();
-
-#define DEBOUNCE_MILLIS 200
-
+// this runs the 8x8 Pimoroni NeoPixel matrix
 Adafruit_NeoMatrix matrix
   = Adafruit_NeoMatrix(8, 8, 2,
                        NEO_MATRIX_TOP  + NEO_MATRIX_RIGHT +
@@ -44,12 +41,13 @@ const uint16_t colours[] = {
   matrix.Color(255, 127, 0),
   matrix.Color(255, 0, 127),
   matrix.Color(0, 127, 255),
-  
+
   matrix.Color(127, 0, 255),
   matrix.Color(0, 255, 127),
   matrix.Color(127, 255, 0),
 };
 
+// co-ords of 8x8 matrix ordered as clockwise spiral 
 const uint8_t spiral[][2] = {
   {3, 3}, {4, 3},
   {4, 4}, {3, 4}, {2, 4}, {2, 3},
@@ -58,48 +56,31 @@ const uint8_t spiral[][2] = {
   {1, 1}, {2, 1}, {3, 1}, {4, 1}, {5, 1}, {6, 1}, {6, 2}, {6, 3}, {6, 4}, {6, 5},
   {6, 6}, {5, 6}, {4, 6}, {3, 6}, {2, 6}, {1, 6}, {0, 6}, {0, 5}, {0, 4}, {0, 3}, {0, 2}, {0, 1},
   {0, 0}, {1, 0}, {2, 0}, {3, 0}, {4, 0}, {5, 0}, {6, 0}, {7, 0}, {7, 1}, {7, 2}, {7, 3}, {7, 4}, {7, 5}, {7, 6},
-  {7, 7}, {6, 7}, {5, 7}, {4, 7}, {3, 7}, {2, 7}, {1, 7}, {0, 7}
+  {7, 7}, {6, 7}, {5, 7}, {4, 7}, {3, 7}, {2, 7}, {1, 7}, {0, 7}, {0, 7}
 };
 
 
 #define PIR 3       // pin connected to Passive InfraRed sensor
 #define BUTTON 10
-#define LED 13
 
-volatile byte money = 250;
-volatile boolean showing = false;
+uint8_t money = 250;
+int8_t pixel = 0;
+
+volatile boolean movement_detected = false;
+boolean animation_running = false;
+boolean animation_growing = true;
 
 void pir_on() {
-  if (money < 1 or showing) {
-    return;
-  }
-
-  showing = true;
-  for (byte t = 0; t < 20; t++) {
-    for (byte i = 0; i < 64; i++) {
-      // matrix.drawPixel(spiral[i][0], spiral[i][1], random(65536));
-      matrix.drawPixel(spiral[i][0], spiral[i][1], colours[random(12)]);
-      matrix.show();
-    }
-  }
-  matrix.fillScreen(0);
-  matrix.show();
-  money--;
-  showing = false;
+  movement_detected = true;
 }
 
-void pir_off() {
-  digitalWrite(LED, LOW);
-}
 
 void setup() {
   // set interrupt service routine for passive infra-red sensor
   pinMode(PIR, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(PIR), pir_on, RISING);
-  //attachInterrupt(digitalPinToInterrupt(PIR), pir_off, FALLING);
 
   pinMode(BUTTON, INPUT);
-  pinMode(LED, OUTPUT);
 
   matrix.begin();
   matrix.setBrightness(4);
@@ -109,27 +90,46 @@ void setup() {
   lcd.init();
   lcd.display();
   lcd.backlight();
+  show_money(money);
 }
 
 void loop() {
-  /* add money when button is pressed
-    if (digitalRead(BUTTON) and millis() - button_press_millis > DEBOUNCE_MILLIS) {
-    button_press_millis = millis();
-    if (money < 246) {
-      money += 10;
-    }
-    }
-  */
-  if (digitalRead(BUTTON)) {
-    money = 250;
+  // add money when button is pressed
+  if (digitalRead(BUTTON) and money < 255) {
+    show_money(++money);
   }
-  show_money();
+
+  if (movement_detected and ! animation_running) {
+    show_money(--money);
+    animation_running = true;
+    animation_growing = true;
+    pixel = 0;
+  }
+
+  if (animation_running) {
+    matrix.drawPixel(spiral[pixel][0], spiral[pixel][1],
+                     animation_growing ? colours[random(12)] : 0
+                    );
+    matrix.show();
+
+    animation_growing ? pixel++ : pixel--;
+
+    if (pixel == 64) {
+      animation_growing = false;
+    }
+
+    if (pixel < 0) {
+      animation_running = false;
+      movement_detected = false;
+    }
+  }
+  delay(50);
 }
 
 
-void show_money() {
+void show_money(int money) {
   lcd.setCursor(0, 0);
-  lcd.print("balance ");
+  lcd.print("Balance ");
   if (money < 0) {
     lcd.print("-");
   }
@@ -137,7 +137,8 @@ void show_money() {
 
   lcd.setCursor(0, 1);
   money < 1 ?
-  lcd.print("seek assistance ") :
+  lcd.print("Seek assistance ") :
   lcd.print("                ");
 }
+
 
