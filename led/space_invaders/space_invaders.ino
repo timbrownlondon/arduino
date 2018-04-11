aa5da2fd9b63fcf4214c35b53634d596b8ba96fe
Tim Brown <tim@redpill.me.uk>
2018-04-11 22:25:16 +0100

    add interrupt for micro-switch to trigger animation invert sprites as installing matrices other way up

--- led/space_invaders/space_invaders.ino
+++ led/space_invaders/space_invaders.ino
@@ -6,37 +6,46 @@
   typically they are chained together in up to 8 devices
 */
 
-LedControl lc = LedControl( 5, 7, 6, 4);
+LedControl lc = LedControl( 5, 4, 6, 4); // data, clock, device select
 
 byte sprites[2][2][8] = {
   {
-    {24, 60, 126, 219, 255, 36, 90, 165}, {24, 60, 126, 219, 255, 36, 90, 66} // squid
+    {165, 90, 36, 255, 219, 126, 60, 24}, {66, 90, 36, 255, 219, 126, 60, 24} // squid
   },
   {
-    {36, 36, 126, 219, 255, 255, 165, 36}, {36, 165, 255, 219, 255, 126, 36, 66} // happy
+    {36, 165, 255, 255, 219, 126, 36, 36}, {66, 36, 126, 255, 219, 255, 165, 36} // happy
   }
 };
 
+#define SWITCH_PIN 2
+#define BOARD_LED 13
 
-unsigned long delays[] = {150, 120, 96, 77, 62, 50, 40, 32, 26, 21, 17, 14, 11, 9, 7, 5, 4 , 3};
-byte speed = 0; // slowest
 
-byte toggle = 0;
-byte x = 0;
-byte direction = 1;
 byte sprite_index = 0;
 
+volatile boolean switch_triggered = false;
+
 void setup() {
-  int devices = lc.getDeviceCount();
-  // init all devices
-  for (int address = 0; address < devices; address++) {
+
+  // initialise all led matrices
+  for (int i = 0; i < lc.getDeviceCount(); i++) {
     /*The MAX72XX is in power-saving mode on startup*/
-    lc.shutdown(address, false);
+    lc.shutdown(i, false);
     /* Set the brightness 0 - 15 */
-    lc.setIntensity(address, 1);
+    lc.setIntensity(i, 1);
     /* and clear the display */
-    lc.clearDisplay(address);
+    lc.clearDisplay(i);
   }
+
+  // attach interrupt function, click(), to the micro switch
+  pinMode(SWITCH_PIN, INPUT);
+  attachInterrupt(digitalPinToInterrupt(SWITCH_PIN), click, RISING);
+  pinMode(BOARD_LED, OUTPUT);
+}
+
+void click() {
+  switch_triggered = true;
+  digitalWrite(BOARD_LED, HIGH);
 }
 
 
@@ -47,7 +56,7 @@ void show_sprite_at(byte location, byte sprite[2][8], byte toggle) {
 }
 
 
-void show_sprite_at_x(byte x, byte sprite[2][8], byte toggle) {
+void ___show_sprite_at_x(byte x, byte sprite[2][8], byte toggle) {
   byte addr = (31 - x) / 8;
   byte offset = x % 8;
   for (byte row = 0; row < 8; row++) {
@@ -57,19 +66,31 @@ void show_sprite_at_x(byte x, byte sprite[2][8], byte toggle) {
   }
 }
 
+void show_sprite_at_x(byte x, byte sprite[8]) {
+  byte matrix = x / 8;
+  byte offset = x % 8;
+  for (byte row = 0; row < 8; row++) {
+    lc.spiTransfer(matrix, row + 1, sprite[row] << offset);
+    lc.spiTransfer(matrix + 1, row + 1, sprite[row] >> (8 - offset));
+    lc.clearDisplay(matrix - 1);
+  }
+}
 
 void loop() {
-  show_sprite_at_x(x, sprites[sprite_index], toggle);
-  delay(delays[speed]);
-
-  toggle = (toggle + 1) % 2;
-  x += direction;
-
-  if (x == 0 or x == 24) {
-    direction *= -1;
+  if (switch_triggered) {
+    run_animation();
+    switch_triggered = false;
   }
-  if (x == 0) {
-    sprite_index = (sprite_index + 1) % 2;
-    speed = (speed + 1) % 18;
+  delay(20);
+}
+
+void run_animation() {
+
+  for (byte x = 0; x < 33; x++) {
+    show_sprite_at_x(x, sprites[sprite_index][ x % 2]);
+    delay(500);
+    digitalWrite(BOARD_LED, LOW);
+
   }
+  sprite_index = (sprite_index + 1) % 2;
 }
