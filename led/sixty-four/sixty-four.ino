1ba9c3d40a973c2c5ed6039e2d5531b3b1f748bb
Tim Brown <tim@redpill.me.uk>
2024-02-07 20:21:53 +0000

    minor change from 2019

--- led/sixty-four/sixty-four.ino
+++ led/sixty-four/sixty-four.ino
@@ -6,14 +6,14 @@
   typically they are chained together in up to 8 devices
 */
 
-LedControl lc = LedControl( 4, 5, 6, 1); // data, clock, device select, number of devices
+LedControl lc = LedControl( 4, 5, 6, 8); // data, clock, device select, number of devices
 
 void setup() {
   for (int i = 0; i < lc.getDeviceCount(); i++) {
     /*The MAX719 is in power-saving mode on startup*/
     lc.shutdown(i, false);
     /* Set the brightness 0 - 15 */
-    lc.setIntensity(i, 1);
+    lc.setIntensity(i, 4);
     /* and clear the display */
     lc.clearDisplay(i);
   }
