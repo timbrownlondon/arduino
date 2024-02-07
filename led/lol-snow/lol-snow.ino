f905326493a7cef562a5ed70f10e70e83f5c8698
Tim Brown <tim@redpill.me.uk>
2024-02-07 18:51:20 +0000

    mnor changes to lol-snow.ino (from 2019)

--- led/lol-snow/lol-snow.ino
+++ led/lol-snow/lol-snow.ino
@@ -5,32 +5,21 @@ byte cell[DISPLAY_COLS][DISPLAY_ROWS];
 void setup() {
   LedSign::Init(GRAYSCALE);  //Initializes the screen
   for (byte x = 0; x < DISPLAY_COLS; x++) {   // 14 columns
-    for (byte y = 0; y <= DISPLAY_ROWS; y++) { // 9 rows
+    for (byte y = 0; y < DISPLAY_ROWS; y++) { // 9 rows
       cell[x][y] = pix();
     }
   }
 }
 
-boolean on = false;
-byte chance = 5;
-byte delta = 1;
-
 void loop() {
-  chance += delta;
-  if(chance > 50){
-    delta = -1;
-  }
-  if(chance < 5){
-    delta = 1;
-  }
   for (byte y = 0; y < DISPLAY_ROWS; y++) { // 9 rows
     for (byte x = 0; x < DISPLAY_COLS; x++) {   // 14 columns
-      LedSign::Set(x, y, cell[x][y]);
-      if (y == DISPLAY_ROWS - 1) {
+      LedSign::Set(x, y, cell[DISPLAY_COLS - (x + 1)][y]);
+      if (x == DISPLAY_COLS - 1) {
         cell[x][y] = pix();
       }
       else {
-        cell[x][y] = cell[x][y + 1];
+        cell[x][y] = cell[x + 1][y];
       }
     }
   }
@@ -38,7 +27,7 @@ void loop() {
 }
 
 byte pix() {
-  if (random(chance)) return 0;
-  return random(2) + 1;
+  if (random(10)) return 0;
+  return random(3);
 }
 
